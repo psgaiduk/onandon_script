04a08281c7566fdb52b9a1cfb536301534465e86
pavel <psgaiduk@gmail.com>
2022-01-06 16:14:44 +0300

    add sound in long time

--- usual_func.py
+++ usual_func.py
@@ -1,13 +1,20 @@
 from sound_func import small_beep, long_beep, last_second
 import time
 
+
 def countdown(time_minutes, text):
     input(f'{text} {time_minutes} минут, жми enter')
+    need_times = [time_minutes // i for i in range(2, 5)]
     for i in range(time_minutes):
         print(f'{time_minutes - i} минут осталось')
         time.sleep(60)
+        if i in need_times:
+            small_beep()
+        if i == 1:
+            last_second()
     long_beep()
     print()
+
 
 def next_step(text):
     input(f'{text}, чтобы продолжить жми enter')
