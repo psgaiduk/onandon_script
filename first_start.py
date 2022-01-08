7e2ee8e637a12f5f2ec53b597ee3303cca63f14d
psgaiduk <65785004+psgaiduk@users.noreply.github.com>
2022-01-08 12:04:47 +0300

    add first start

--- first_start.py
+++ first_start.py
@@ -13,23 +13,21 @@ def check_file(name):
     return True
 
 
-check_file('history_user.json')
-was_file = check_file('settings_user.json')
-if not was_file:
-    name_user = input('Введите ваше имя: ')
-    affirmacii = input('Введите ваши аффирмации через, разделяя их ; ')
-    name_growing = input('Введите как вы будете саморазвиваться по утрам (чтение, решение задача и т.д.) ')
-    name_art = input('Введите каким творчеством вы будете заниматься по вечерам (рисование, музыка и т.д.) ')
-    with open('settings_user.json', 'w+', encoding='utf-8') as file:
-        data = {
-            'name_user': name_user,
-            'affirmacii': affirmacii.split(';'),
-            'name_growing': name_growing,
-            'name_art': name_art,
-            'date_start': date.today().strftime('%Y-%m-%d'),
-            'level': 1
-        }
-        json.dump(data, file)
-
-with open('settings_user.json') as file:
-    print(json.load(file))
+def start_work():
+    check_file('history_user.json')
+    was_file = check_file('settings_user.json')
+    if not was_file:
+        name_user = input('Введите ваше имя: ')
+        affirmacii = input('Введите ваши аффирмации через, разделяя их ; ')
+        name_growing = input('Введите как вы будете саморазвиваться по утрам (чтение, решение задача и т.д.) ')
+        name_art = input('Введите каким творчеством вы будете заниматься по вечерам (рисование, музыка и т.д.) ')
+        with open('settings_user.json', 'w+', encoding='utf-8') as file:
+            data = {
+                'name_user': name_user,
+                'affirmacii': affirmacii.split(';'),
+                'name_growing': name_growing,
+                'name_art': name_art,
+                'date_start': date.today().strftime('%Y-%m-%d'),
+                'level': 0
+            }
+            json.dump(data, file)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from evening import start_evening
 from morning import start_morning
+from first_start import start_work
+
+
+start_work()
 
 if datetime.now().hour > 18:
     start_evening()
